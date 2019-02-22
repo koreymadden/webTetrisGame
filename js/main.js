a88d3b198b6dea6a95a996732bcd6016b941b119
Korey <kmmadden@iupui.edu>
2019-02-21 19:17:01 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".dropdown-trigger").dropdown({
+    coverTrigger: false,
+    inDuration: 300,
+    outDuration: 225,
+    hover: false,
+});
+$(".dropdown-content>li>a").css("color", "#424242" );
+$('.sidenav').sidenav();
