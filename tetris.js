207676a1ec0515509cf90dda724e63b97349adb0
Korey <kmmadden@iupui.edu>
2019-02-21 21:54:52 -0500

    More progress after first commit...

--- tetris.js
+++ tetris.js
@@ -3,15 +3,28 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000'
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
-
+// Don't understand
+function createMatrix (w, h) {
+    const matrix = [];
+    while (h--)  {
+        matrix.push(new Array(w).fill(0))
+    }
+    return matrix;
+}
+// Drawing background and updating player
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
+// Drawing player part based on matrix shape and location
 function drawMatrix(matrix, offset) {
     matrix.forEach(function (row, y) {
         row.forEach(function (value, x) {
@@ -23,4 +36,52 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-drawMatrix(matrix, {x: 5, y: 5});
+function merge(arena, player) {
+    player.matrix.forEach(function (row, y) {
+        row.forEach(function (value, x) {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+// Updating players location downwards and resetting drop timer
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0;
+// Creating logic for when the player will drop and updating screen
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        playerDrop();
+    }
+    draw();
+    requestAnimationFrame(update);
+}
+
+const arena = createMatrix(12, 20);
+// Player starting location
+const player = {
+    pos: {x: 5, y: 5},
+    matrix: matrix,
+};
+// Adding player controls
+document.addEventListener('keydown', function (event) {
+    console.log(event);
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39) {
+        player.pos.x++;
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+});
+// Starting the game
+update();
