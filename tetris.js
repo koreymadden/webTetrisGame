839d394b9ff8db4fb6b7f8c3480734e5b8b48a93
Korey <kmmadden@iupui.edu>
2019-02-22 22:47:21 -0500

    More progress, got to vertical collision...

--- tetris.js
+++ tetris.js
@@ -1,16 +1,27 @@
+// Getting HTML elements
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
-
+// Make game more viewable and match canvas.height & width
 context.scale(20, 20);
-
-
-
+// Players game piece
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
-// Don't understand
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+// Creates dimensions of game in zeros (Going to be 20 Arrays with 12 zeros in each array)
 function createMatrix (w, h) {
     const matrix = [];
     while (h--)  {
@@ -18,13 +29,14 @@ function createMatrix (w, h) {
     }
     return matrix;
 }
-// Drawing background and updating player
+// Drawing background and updating player & existing blocks
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 }
-// Drawing player part based on matrix shape and location
+// Drawing player blocks based on matrix shape and location
 function drawMatrix(matrix, offset) {
     matrix.forEach(function (row, y) {
         row.forEach(function (value, x) {
@@ -32,15 +44,15 @@ function drawMatrix(matrix, offset) {
                 context.fillStyle = 'red';
                 context.fillRect(x + offset.x, y + offset.y, 1, 1)
             }
-        })
+        });
     });
 }
-
+// Taking the player's game piece and copying it onto the arena's matrix
 function merge(arena, player) {
     player.matrix.forEach(function (row, y) {
         row.forEach(function (value, x) {
             if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
+                arena[player.pos.y + y][player.pos.x + x] = value;
             }
         });
     });
@@ -48,6 +60,11 @@ function merge(arena, player) {
 // Updating players location downwards and resetting drop timer
 function playerDrop() {
     player.pos.y++;
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
 }
 
@@ -65,11 +82,11 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
-
+// Creating space so that blocks do not go out of viewable area
 const arena = createMatrix(12, 20);
 // Player starting location
 const player = {
-    pos: {x: 5, y: 5},
+    pos: {x: 0, y: 0},
     matrix: matrix,
 };
 // Adding player controls
@@ -85,3 +102,10 @@ document.addEventListener('keydown', function (event) {
 });
 // Starting the game
 update();
+
+
+// Testing...
+let x = 5;
+while (x--) {
+    console.log(x);
+}
